562977cc00823a55b6ac4fbe466fb8de43ec2a8e
gracaninja <gracaninja@gmail.com>
2014-07-04 17:01:14 +0100

    new updates to new architecture

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='unbabel-py',
-      version='0.28',
+      version='0.29',
       description='Python Wrapper around Unbabel HTTP API',
       author='Joao Graca',
       author_email='gracaninja@unbabel.co',
