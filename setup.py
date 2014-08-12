56ec95f220d35e08f4c8ee9fae998f6a7bc31f0f
gracaninja <gracaninja@gmail.com>
2014-08-12 16:11:53 +0100

    fixed broken argument ttype to type

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='unbabel-py',
-      version='0.33',
+      version='0.34',
       description='Python Wrapper around Unbabel HTTP API',
       author='Joao Graca',
       author_email='gracaninja@unbabel.co',
