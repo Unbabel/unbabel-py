d05213236b2968684c4ae69d567e0cdb7003fc95
Sam <sam@unbabel.com>
2014-09-15 19:55:54 +0100

    www.unbabel.com -> unbabel.com

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='unbabel-py',
-      version='0.36',
+      version='0.37',
       description='Python Wrapper around Unbabel HTTP API',
       author='Joao Graca',
       author_email='gracaninja@unbabel.co',
