df62d5dd0fc2696a7f5e0725daf1c3588a062dea
Hugo Silva <hugo.f.v.silva@gmail.com>
2017-06-09 10:46:15 +0100

    Bump version to 0.49

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup
 
 setup(name='unbabel-py',
-      version='0.46',
+      version='0.49',
       description='Python Wrapper around Unbabel HTTP API',
       author='Joao Graca',
       author_email='gracaninja@unbabel.co',
       packages=[
           'unbabel',
-          ],
+      ],
       package_dir={
-        'unbabel': 'unbabel',
-        },
+          'unbabel': 'unbabel',
+      },
       install_requires=[
-        'requests',
-        'beautifulsoup4',
-          ],
-      url = 'https://github.com/Unbabel/unbabel-py',
-      download_url = 'https://github.com/Unbabel/unbabel-py/tarball/0.1',
-      classifiers = ['Development Status :: 4 - Beta',
-                     'Intended Audience :: Developers',
-                     'Programming Language :: Python ',
-                     'Topic :: Text Processing'
-                     ]
+          'requests',
+          'beautifulsoup4',
+      ],
+      url='https://github.com/Unbabel/unbabel-py',
+      download_url='https://github.com/Unbabel/unbabel-py/tarball/0.1',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Developers',
+                   'Programming Language :: Python ',
+                   'Topic :: Text Processing'
+                   ]
       )
